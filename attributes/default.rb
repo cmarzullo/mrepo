f2e9deedeccc3f4bd717604f64b17eba6e088f2e
Chris Marzullo <sevenmanin@gmail.com>
2014-10-18 21:45:47 -0700

    httpd working

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,3 +8,17 @@ default[:mrepo][:config][:smtpserver] = 'localhost'
 default[:mrepo][:config][:cron] = nil
 default[:mrepo][:config][:httpd_enabled] = true
 
+# These are the default mods under CentOS
+default[:mrepo][:config][:httpd_vars][:DocumentRoot] = default[:mrepo][:config][:wwwdir] 
+default[:mrepo][:config][:httpd_mods] = %w{
+                                          actions alias auth_basic auth_digest authn_alias
+                                          authn_anon authn_dbm authn_default authn_file
+                                          authnz_ldap authz_dbm authz_default authz_groupfile
+                                          authz_host authz_owner authz_user autoindex cache
+                                          cgi dav_fs dav deflate dir disk_cache env expires
+                                          ext_filter headers include info ldap log_config logio
+                                          mime mime_magic negotiation proxy_ajp proxy_balancer
+                                          proxy_connect proxy_ftp proxy_http proxy rewrite
+                                          setenvif speling status substitute suexec userdir
+                                          usertrack version vhost_alias
+                                        }
--- recipes/_httpd.rb
+++ recipes/_httpd.rb
@@ -1,8 +1,17 @@
 # Setup a web server
 #
 
+
+httpd_config 'mrepo' do
+  source 'mrepo.httpd.conf.erb'
+  action :create
+  instance 'mrepo'
+end
+
 httpd_service 'mrepo' do
   servername 'localhost'
   action :create
   keepalive true
+  instance 'mrepo'
+  modules node[:mrepo][:config][:httpd_mods]
 end
